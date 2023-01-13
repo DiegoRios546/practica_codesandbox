ad76aa6b8df1d8842f835af334aaf02e35af273b
DiegoRios546 <118926566+DiegoRios546@users.noreply.github.com>
2023-01-13 14:38:28 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Mi practica con codesandbox</h1>
+      <h2>Diego Alexis Rios Torres</h2>
+      <h3>Universidad Tecnologica de Durango</h3>
+      <p>My git hub</p>
+    </div>
+  );
+}
